c562152766689849655f6ee61c60f6d81a26c9c2
Andy Sonnenburg <andy22286@gmail.com>
2014-02-16 22:37:23 -0600

    cyclic shared pointers

--- include/shared/cyclic.hpp
+++ include/shared/cyclic.hpp
@@ -3,41 +3,190 @@
 
 namespace shared {
 	namespace cyclic {
+
+		namespace parent {
+			template <typename T>
+			class traits {
+			public:
+				typedef typename T::children_type children_type;
+			};
+		}
+
+		template <typename T>
+		typename parent::traits<T>::children_type children(T const&);
+
 		namespace detail {
 			enum class color {
-				Black,
-				Gray,
-				White
+				black,
+				gray,
+				white
 			};
 
-			template <typename T, typename Count>
-			class counted {
-				T value_;
-				Count count_;
+			typedef unsigned int count;
 
+			template <typename T>
+			class counted {
 			public:
+				T value_;
+				color color_;
+				count count_;
+
 				template <typename... Args>
 				counted(Args&&... args):
 					value_(std::forward<Args>(args)...),
+					color_(color::black),
 					count_(1) {}
 
-				T& value() {
-					return value_;
+				void collect_cycles() {
+					mark_gray();
+					scan();
+					collect_white();
+				}
+
+				void mark_gray() {
+					if (color_ != color::gray) {
+						color_ = color::gray;
+						for (auto& ptr: children(value_)) {
+							auto t = ptr.counted_;
+							if (t != nullptr) {
+								--t->count_;
+								t->mark_gray();
+							}
+						}
+					}
+				}
+
+				void scan() {
+					if (color_ == color::gray) {
+						if (count_ > 0) {
+							scan_black();
+						} else {
+							color_ = color::white;
+							for (auto& ptr: children(value_)) {
+								ptr.scan();
+							}
+						}
+					}
+				}
+
+				void scan_black() {
+					color_ = color::black;
+					for (auto& ptr: children(value_)) {
+						auto t = ptr.counted_;
+						if (t != nullptr) {
+							++t->count_;
+							if (t->color_ == color::black) {
+								t->scan_black();
+							}
+						}
+					}
+				}
+
+				void collect_white() {
+					if (color_ == color::white) {
+						for (auto& ptr: children(value_)) {
+							ptr.collect_white();
+						}
+					}
 				}
 			};
 		}
 
-		template <typename T, typename Count = unsigned int>
+		template <typename T>
 		class ptr {
-			detail::counted<T, Count>* counted_;
+			detail::counted<T>* counted_;
 			T* ptr_;
 
 		public:
 			template <typename... Args>
 			explicit ptr(Args&&... args):
-				counted_(new detail::counted<T, Count>(std::forward<Args>(args)...)),
-				ptr_(&counted_->value()) {}
+				counted_(new detail::counted<T>(std::forward<Args>(args)...)),
+				ptr_(&counted_->value_) {}
+
+			ptr& operator=(ptr const& rhs) {
+				return operator=<T>(rhs);
+			}
+
+			template <typename U>
+			ptr& operator=(ptr<U> const& rhs) {
+				if (counted_ == rhs.counted_) {
+					return *this;
+				}
+				decrement();
+				counted_ = rhs.counted_;
+				ptr_ = rhs.ptr_;
+				if (counted_ != nullptr) {
+					++counted_->count_;
+				}
+				return *this;
+			}
+
+			~ptr() {
+				decrement();
+			}
+
+			T& operator*() const {
+				return *ptr_;
+			}
+
+		private:
+			void decrement() {
+				auto counted = counted_;
+				if (counted != nullptr) {
+					if (--counted->count_ == 0) {
+						release();
+					} else {
+						counted_->collect_cycles();
+					}
+				}
+			}
+
+			void release() {
+				delete counted_;
+			}
+
+			void scan() {
+				if (counted_ != nullptr) {
+					counted_->scan();
+				}
+			}
+
+			void collect_white() {
+				if (counted_ != nullptr) {
+					auto counted = counted_;
+					counted_ = nullptr;
+					counted->collect_white();
+					delete counted;
+				}
+			}
+
+			template <typename U> friend class detail::counted;
 		};
+
+		namespace parent {
+			template <typename T>
+			class empty {
+			public:
+				ptr<T>* begin() {
+					return nullptr;
+				}
+
+				ptr<T>* end() {
+					return nullptr;
+				}
+			};
+
+			template <>
+			class traits<int> {
+			public:
+				typedef empty<int> children_type;
+			};
+		}
+
+		template <>
+		parent::traits<int>::children_type children<int>(int const&) {
+			return parent::empty<int>();
+		}
 	}
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,10 @@
 #include "shared.hpp"
 
 int main() {
-	using namespace shared;
-	acyclic_ptr<int> x(1);
-	acyclic_ptr<int> y(2);
-	acyclic_ptr<int> z(3);
+	using namespace shared::cyclic;
+	ptr<int> x(1);
+	ptr<int> y(2);
+	ptr<int> z(3);
 	y = x;
 	y = z;
 	std::cout << *y << std::endl;
