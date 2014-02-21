6b4cc5c7d9af7cbc76434154d3385a0bc367a9ce
Andy Sonnenburg <andy22286@gmail.com>
2014-02-21 14:50:51 -0600

    generalized for_each

--- src/main.cpp
+++ src/main.cpp
@@ -1,51 +1,31 @@
 #include <array>
-#include "shared.hpp"
+#include <iostream>
+#include <tuple>
+#include <vector>
 
-using namespace shared::cyclic;
+#include "memory/traceable.hpp"
 
-struct link {
-	typedef link const& children_type;
-	ptr<link> value;
-	
-	struct end_iterator {
-		ptr<link> operator*() const {
-			return ptr<link>();
+namespace memory {
+	namespace traceable {
+		namespace extension {
+			template <typename T>
+			struct tag_of<std::vector<T>> {
+				typedef container_tag type;
+			};
 		}
-		end_iterator next() const {
-			return end_iterator();
-		}
-	};
-
-	end_iterator end() const {
-		return end_iterator();
 	}
-
-	struct value_iterator {
-		ptr<link> const& value_;
-		value_iterator(ptr<link> const& value): value_(value) {}
-		ptr<link> const& operator*() const {
-			return value_;
-		}
-		end_iterator next() const {
-			return end_iterator();
-		}
-	};
-
-	value_iterator begin() const {
-		return value_iterator(value);
-	}
-};
-
-bool operator!=(link::end_iterator const&, link::end_iterator const&) {
-	return false;
-}
-
-bool operator!=(link::value_iterator const&, link::end_iterator const&) {
-	return true;
 }
 
 int main() {
-	ptr<link> x = make<link>();
-	x->value = x;
+	using namespace memory::traceable;
+
+	std::vector<int> xs { 1, 2, 3 };
+	for_each(xs, [](int x) {
+			std::cout << x << std::endl;
+		});
+	std::tuple<int, int, int> ys { 1, 2, 3 };
+	for_each(ys, [](int y) {
+			std::cout << y << std::endl;
+		});
 	return 0;
 }
